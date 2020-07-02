d10b25c943b5a76464b876e6a285090fdb349fec
PhantomOz <faniogor@gmail.com>
2020-07-02 16:55:51 +0100

    Cli Calculator

---
+++ ucalc.py
@@ -0,0 +1,10 @@
+#buiding a cli calculator
+#import math
+from math import pi, sin, cos, cosh, sinh, tan, tanh, log, log10, sqrt
+calc = True
+while calc:
+    print("GUIDELINES\n * use pi as pi")
+    #input
+    inp = float(input("Write your equation: "))
+    #output
+    print(inp)
